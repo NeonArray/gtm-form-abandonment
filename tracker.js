16e340d2f933c278119769ee684a5c47c8225ea8
Aaron Arney <ocularrhythm@users.noreply.github.com>
2016-08-30 16:36:12 -0400

    Init commit

---
+++ tracker.js
@@ -0,0 +1,62 @@
+var FormAbandonmentTracker = (function () {
+  "use strict";
+
+  var fieldsAccessed = []; // An array to hold the fields that the user focused on
+  var didSubmit = false; // only want to send data to Google if the form wasn't submitted
+
+  var createFormEventListeners = function (formSelector) {
+    var elForm = document.querySelector(formSelector);
+
+    // if the form selector is invalid, exit from the function
+    // might move this into #init()
+    if (!elForm) {
+      return;
+    }
+
+    var elInputs = elForm.querySelectorAll("input[type='text']");
+    var i;
+
+    for (i = 0; i < elInputs.length - 1; i = i + 1) {
+      // attach event listeners to valid form fields
+      elInputs[i].addEventListener("focus", function (e) {
+        // record which fields have been focused and store them in array
+        fieldsAccessed.push(this.name);
+      });
+    }
+
+    // attach event handler to form submit
+    elForm.addEventListener("submit", function () {
+      didSubmit = true;
+    });
+  };
+
+  var createWindowEventListener = function () {
+    // if the form is not submitted before the page is left
+    window.onbeforeunload = function () {
+      if (!didSubmit) {
+        // send the event to Google
+        pushDataToGoogle();
+      }
+    }
+  };
+
+
+  var pushDataToGoogle = function () {
+    // data is outside for debug purposes
+    var data = {
+      'event':  'formAbandonment',
+      'eventCategory': 'Form Abandonment',
+      'eventAction': fieldsAccessed.join(' > ')
+    };
+    window.dataLayer.push(data);
+  };
+
+  return {
+    init: function (formSelector) {
+      createFormEventListeners(formSelector);
+      createWindowEventListener();
+    }
+  };
+}());
+
+FormAbandonmentTracker.init(".formClass");
